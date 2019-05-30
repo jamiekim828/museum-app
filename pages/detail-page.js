23a88fd061ae83b677f6c6e1e653d62ebe9d08e7
jamiekim828 <jamiekim828@gmail.com>
2019-05-30 21:05:21 +0200

    Set up js files and declare submitComment

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,3 @@
 function submitComment() {
-    const inputField = document.getElementById('name')
-    const name = inputField.nodeValue
-    const textArea = document.getElementById('msg')
-    const msg = testArea.value 
-    const comment = document.createElement('section')
-    const h3 = dcument.createElement('h3')
-    const p = document.createElament('p')
-    h3.innerHTML = '${name} said:'
-    p.innerHTML = msg
-    comment.classList.add('comment')
-    comment.appendChild(h3)
-    comment.appendChild(p)
-    console.log(msg)
-}
+    console.log('Hello!');
+};
