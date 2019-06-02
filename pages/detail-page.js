0b77224617771a43aab7918bcee84c9b3526d885
jamiekim828 <jamiekim828@gmail.com>
2019-06-03 01:24:19 +0200

    user input

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,13 +1,26 @@
 function submitComment() {
-    const inputField = document.getElementById('name');
-    const name = inputField.value;
-    const textArea = document.getElementById('msg');
-    const msg = textArea.value;
-    const comment = document.createElement('section');
-    const h3 = document.createElement('h3');
-    const p = document.createElement('p');
-    h3.innerHTML ='${name} said:';
-    p.innerHTML = msg;
-    comment.appendChild(h3);
-    comment.appendChild(p);
-};
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    const commentSection = document.getElementById('newbox')
+    commentSection.appendChild(comment)
+}
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+      alert('You forgot to fill in your name or message!')
+      return true;
+    }
+  
+    if(msg.length > 280) {
+      alert('Your comment is too long')
+      return true
+    }
+}
