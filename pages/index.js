d7226bf82134384181acdc74d13232375fb5da9e
jamiekim828 <jamiekim828@gmail.com>
2019-06-02 13:23:20 +0200

    add js code in the first page

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+const data = {
+    elapsedMilliseconds: 0,
+    count: 359,
+    countFacets: {
+      hasimage: 357,
+      ondisplay: 43
+      },
+    artObjects: {
+        web: "https://www.alletop10lijstjes.nl/wp-content/uploads/2016/12/Campbells-Soup-Cans.jpg",
+        id: "01",
+        title: "Campbell's Soup Cans",
+        hasImage: true,
+        principalOrFirstMaker: "Andy Warhol",
+        showImage: false,
+        permitDownload: false
+        }
+}
